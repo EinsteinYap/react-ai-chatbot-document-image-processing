7f2db12fbf3317bb88b324d513476b2d1ae67434
EinsteinYap <einsteinyap2014@gmail.com>
2026-05-01 21:02:55 +0800

    npm i buffer and upload function done

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,10 +1,21 @@
+import { Buffer } from 'buffer';
+
 
 function FileUpload() {
+    async function handleFileUpload(event) {
+        const fileUpload = await event.target.files[0].arrayBuffer();
+        const file ={
+            type: event.target.files[0].type,
+            file: Buffer.from(fileUpload).toString('base64')
+        }
+        console.log("File uploaded:", file)
+    }
     return (
         <div>
             <section>
                 <h2>Get Started</h2>
-                <input type="file" accept=".pdf, image/*" />
+                <input type="file" accept=".pdf, .png, .jpg, .jpeg"
+                onChange={ handleFileUpload } />
                 <p>Upload a PDF document or image to get started.</p>
             </section>
         </div>
